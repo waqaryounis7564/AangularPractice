f0d09480fbe05bf9b9c3274ba6346fc88d88ee50
Waqar <waqar.younas7564@gmail.com>
2021-04-18 13:11:55 +0500

    log functionality added by using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,18 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   show: boolean | undefined;
+  total = 0;
+  log = [];
 
   // tslint:disable-next-line:typedef
   onClick() {
+    // this.total = 0;
     this.show = this.show ? false : true;
-    console.log(this.show);
+    // @ts-ignore
+    this.total++;
+    // @ts-ignore
+    this.log.push(this.total);
+    console.log(this.total);
+
   }
 }
