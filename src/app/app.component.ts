8d60ab325704c372c3da453781d9449570751ac4
Waqar <waqar.younas7564@gmail.com>
2021-04-25 12:08:48 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  show: boolean | undefined;
-  total = 0;
-  log = [];
 
-  // tslint:disable-next-line:typedef
-  onClick() {
-    // this.total = 0;
-    this.show = this.show ? false : true;
-    // @ts-ignore
-    this.total++;
-    // @ts-ignore
-    this.log.push(this.total);
-    console.log(this.total);
-
-  }
 }
